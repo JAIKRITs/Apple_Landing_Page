c0b54f59079dcc837dab1f35eb9cda702c66fb0e
RakshaDave <jaikritsanandiya@gmail.com>
2026-02-16 18:30:17 +0530

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar.jsx"
+import Hero from "./components/Hero.jsx"
 
 const App = () => {
   return (
-    <main>
+    <main className="pt-[7vh]">
       <NavBar />
+      <Hero />
     </main>
   )
 }
